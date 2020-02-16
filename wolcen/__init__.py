a12bc24d6fa6354e35f3f115a87b6400c662dddd
fwidm <3480240+FWidm@users.noreply.github.com>
2020-02-16 07:46:04 +0100

    chore(*): documentation

--- wolcen/__init__.py
+++ wolcen/__init__.py
@@ -10,14 +10,23 @@ from wolcen.models.ring import Ring
 from wolcen.models.skill import Skill
 
 
-def translate_ui_string(key: str, source: dict) -> str:
-    # strip leading @
+def translate_ui_string(source: dict, key: str) -> str:
+    """
+    For a given source, obtain the translation value for the given key
+    :param source: dictionary to obtain translation from
+    :param key: key, starting with `@...`, this will be stripped internally for the lookup
+    :return: value if exists else None
+    """
     return source.get(key[1:], None)
 
 
 def parse_effect(effect: untangle.Element):
-    id = effect['Name']
-    text = translate_ui_string(effect['HUDDesc'], effect_translator)
+    """
+    Parse "effects" of nodes, replace placeholders with values
+    :param effect: xml untangle element
+    :return: combined string describing the node
+    """
+    text = translate_ui_string(effect_translator, effect['HUDDesc'])
     # find another way to just get any attributes from untangle...
     kv = effect.Semantics._attributes
 
@@ -34,38 +43,60 @@ def parse_effect(effect: untangle.Element):
 
 
 def parse_skill(skill: untangle.Element, tree_nodes_data: untangle.Element):
+    """
+    Parse a node (skill) in the passive tree
+    :param skill: raw xml of the element
+    :param tree_nodes_data: all specific node data
+    :return: data object holding combined info of available data
+    """
     data = next(node for node in tree_nodes_data.MetaData.Spell if node['Name'] == skill['Name'])
     effects = [parse_effect(effect) for effect in data.MagicEffects.EIM]
-    name = translate_ui_string(data['UIName'], skill_translator)
+    name = translate_ui_string(skill_translator, data['UIName'])
     return Skill(skill['Name'], name, skill['Rarity'], skill['Category'], skill['MaxLevel'], skill['Angle'],
                  skill['Pos'],
                  skill['Unlock'], effects)
 
 
 def parse_ring(tree_name: str, number: int):
-    ring_tree = untangle.parse(f'../input/Umbra/Skills/Trees/PassiveSkills/{tree_name}_tree.xml')
-    tree = ring_tree.MetaData.Tree
+    """
+    Parse a ring tree by obtaining the correct xml for the layout, then the correct XML for the node data
+    :param tree_name: internal name of the tree
+    :param number: of the tree
+    :return: Ring representation including skills
+    """
+    xml_ring_tree = untangle.parse(f'../input/Umbra/Skills/Trees/PassiveSkills/{tree_name}_tree.xml')
+    tree = xml_ring_tree.MetaData.Tree
+
     paths = get_passive_tree_xml_paths()
     tree_path = next(path for path in paths if tree_name.lower() in path.lower())
     if not tree_path:
-        return
+        return None
+
     tree_node_data = untangle.parse(f'../input/Umbra/Skills/Passive/PST/{tree_path}')
-    nodes = []
-    for node in tree.Skill:
-        nodes.append(parse_skill(node, tree_node_data))
-    return Ring(tree_name, translate_ui_string(tree['UIName'], skill_translator), number, nodes)
+    nodes = [parse_skill(node, tree_node_data) for node in tree.Skill]
+
+    return Ring(tree_name, translate_ui_string(skill_translator, tree['UIName']), number, nodes)
 
 
 def parse_sections(ring: untangle.Element):
-    sections = ring.Section
-    return {section['Number']: parse_ring(section['Name'], section['Number']) for section in sections}
+    xml_sections = ring.Section
+    return {section['Number']: parse_ring(section['Name'], section['Number']) for section in xml_sections}
 
 
 def get_passive_tree_xml_paths():
+    """
+    Fetch all specific tree xml paths
+    :return: list of all tree xml paths
+    """
     return os.listdir('../input/Umbra/Skills/Passive/PST')
 
 
 def fetch_translations(path):
+    """
+    Fetch translation pairs via regex, feed them into a dictionary
+    :param path: to the translation file
+    :return: dictionary with the key:translation format
+    """
     with open(path, 'r', encoding='utf8') as f:
         content = f.read()
 
